e67966bbbc50ef827b596a3d6f0fff529b0e09b2
bchwtz <benjamin.buchwitz@gmail.com>
2022-02-15 11:50:30 +0100

    Update

---
+++ R/installation.R
@@ -0,0 +1,23 @@
+
+#' @export
+install_fhswf <- function(){
+  #
+  tinytex::tlmgr_update()
+  tinytex::tlmgr_install("elsarticle")
+  tinytex::tlmgr_install("pdfcolmk")
+  tinytex::tlmgr_install("eurosym")
+  tinytex::tlmgr_install("paralist")
+  tinytex::tlmgr_install("sectsty")
+  tinytex::tlmgr_install("subfig")
+  tinytex::tlmgr_install("lineno")
+  tinytex::tlmgr_install("caption")
+  tinytex::tlmgr_install("algorithm2e")
+  tinytex::tlmgr_install("ifoddpage")
+  tinytex::tlmgr_install("relsize")
+  tinytex::tlmgr_install("setspace")
+  tinytex::tlmgr_install("ifdraft")
+
+  pkg <- c("caption", "xpatch", "csquotes", "verbatimbox")
+  x <- sapply(pkg, tinytex::tlmgr_install)
+}
+
