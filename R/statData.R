1dddcd5d6bc85b9b11c36eff5deb569265b6b6fe
bchwtz <benjamin.buchwitz@gmail.com>
2022-03-31 17:57:15 +0200

    Update

---
+++ R/statData.R
@@ -0,0 +1,8 @@
+#' Data for Statistics 2022
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A list with data
+#' @source \url{https://www.bchwtz.de/}
+"stat2022"
