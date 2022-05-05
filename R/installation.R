49a567e9881171d90c8f257f562e6fd009cff4a3
bchwtz <benjamin.buchwitz@gmail.com>
2022-05-05 12:31:02 +0200

    Update

--- R/installation.R
+++ R/installation.R
@@ -34,16 +34,26 @@ update_fhswf <- function(){
 #' shows an rmarkdown chunk for importing a series of pdf certificates.
 #'
 #' @param dir name of the subdirectory in the rmarkdown document folder, the default folder is called "datacamp"
+#' @param verbose indicates if messages should be outputted on the command line. In the example chunk those information is also included in a pdf. Default is TRUE.
 #' @export
 #' @examples
 #' # Example rmarkdown code chunk
-#' ```{r, echo=FALSE, out.width="24%", fig.show='hold'}
+#' ```{r, echo=FALSE, out.width="24%", fig.show='hold', fig.align = 'left'}
 #' fhswf::include_datacamp()
 #' ```
-include_datacamp <- function(dir="datacamp"){
-  f <- list.files(paste0("./",dir), full.names = T)
-  f <- f[grepl("\\.pdf$", f)]
-  knitr::include_graphics(f)
+include_datacamp <- function(dir = "datacamp", verbose = TRUE){
+  path <- paste0("./",dir)
+  if (!dir.exists(path)){
+    if(verbose) message("fhswf::include_datacamp --> There is no folder named: ", dir)
+  } else {
+    f <- list.files(path, full.names = T)
+    f <- f[grepl("\\.pdf$", f)]
+    if(length(f) == 0){
+      if(verbose) message("fhswf::include_datacamp --> There are no PDFs in the folder: ", dir)
+    } else {
+      knitr::include_graphics(f)
+    }
+  }
 }
 
 
