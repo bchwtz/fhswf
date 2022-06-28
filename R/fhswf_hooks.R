1a9b84c2b7d6b4f21a86a38c3c1e9e4afef66f40
hannahbehrens <behrens.hannah@fh-swf.de>
2022-06-28 08:39:05 +0200

    update - June 28, 2022 - HB

--- R/fhswf_hooks.R
+++ R/fhswf_hooks.R
@@ -1,6 +1,8 @@
 #' @rdname presentation
 #' @export
 fhswf_hooks <- function(){
+  # see: "How can I automate different spacing between text and code blocks in R Markdown?" asked by duckmayr June 19, 2019 13:03, answer by Joris C., June 21, 2019 14:27. URL: https://stackoverflow.com/questions/56704022/how-can-i-automate-different-spacing-between-text-and-code-blocks-in-r-markdown [Accessed: June 23, 2022; 08:38]:
+  # hook_chunk = knitr::knit_hooks$get('chunk')
   hook_chunk = knitr::knit_hooks$get('chunk')
 
   knitr::knit_hooks$set(chunk = function(x, options) {
