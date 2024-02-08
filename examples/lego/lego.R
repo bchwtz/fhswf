77c918d1075bae46273dc36126d9aeef7f98362c
bchwtz <benjamin.buchwitz@gmail.com>
2024-02-08 11:49:34 +0100

    Update data

---
+++ examples/lego/lego.R
@@ -0,0 +1,25 @@
+library(readr)
+library(tidyverse)
+library(readxl)
+
+lego <- read_excel("examples/lego/DiB whole sample Dec 2015 prices.xlsx")
+
+## Drop and rename colums
+
+lego <- lego %>%
+  rename(year = `year of release`) %>%
+  rename(piece_count = `# of pieces`) %>%
+  rename(minifigure_count = `# of minifigures`) %>%
+  rename(price_secondary_new = `Secondary market prices of new sets in 2015`) %>%
+  rename(price_secondary_used = `Secondary market prices of used sets in 2015`) %>%
+  rename(price_release = `Primary market price at release`) %>%
+  rename(age_recommendation = age) %>%
+  rename(size_group = `Size group (1 - Biggest; 4 - Smallest)`)
+
+
+names(lego)
+
+# Saving
+f <- paste0("data/lego.rda")
+save(lego, file = f, compress="xz")
+tools::checkRdaFiles(f)
