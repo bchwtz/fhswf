7896da084a184d8cafd5c339c2ebe4bde7d98656
bchwtz <benjamin.buchwitz@gmail.com>
2024-02-09 10:50:00 +0100

    Update

--- R/lego.R
+++ R/lego.R
@@ -17,7 +17,7 @@
 #'   \item{price_secondary_used}{ Average price in US dollars of a used set in online markets. }
 #'   \item{price_release}{ Price in US dollars of the set at release, als suggested by the manufacturer (MSRP). }
 #'   \item{age_recommendation}{ Recommended minimum age of the set. }
-#'   \item{size_group}{ Indicator showing the size group of the set, where 1 is smalels and 4 is largest set size category. }
+#'   \item{size_group}{ Indicator showing the size group of the set, where 1 is smallest and 4 is largest set size category. }
 #' }
 #'
 #' @source \url{https://data.mendeley.com/datasets/v9hhs66vm3/1}
